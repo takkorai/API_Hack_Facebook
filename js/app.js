fbd1e46450c39b4dea6e6b97efca84c4a0f5d805
takkorai <takkorai@gmail.com>
2014-06-23 12:58:19 -0400

    includes image div without image csspush

--- js/app.js
+++ js/app.js
@@ -7,8 +7,29 @@ $(document).ready( function() {
 
     for(var pl in allGolfers){
         $(".players").append('<li class="' + pl + '">'+allGolfers[pl].golfer+'</li>');
-    }
+        $(".players_picture").append('<li class="' + pl + ' '+pl+'_default_picture player_default_picture hidden">&nbsp;</li>');
+        var CurrentPlayersImageClass= (pl+'_default_picture player_default_picture');
 
+        //$(".CurrentPlayersImageClass").css('background-image', 'url(' + img/adam_scott.jpg + ')');
+    };
+
+     $(".players").on("mouseover", "li", function(){
+
+        var hoveredPlayer = $(this).attr("class");
+        console.log(hoveredPlayer);
+        $(".players_picture").find('.'+hoveredPlayer+'').removeClass("hidden")
+        
+     });
+     $(".players").on("mouseout", "li", function(){
+
+        var hoveredPlayer = $(this).attr("class");
+        console.log(hoveredPlayer);
+        $(".players_picture").find('.'+hoveredPlayer+'').addClass("hidden")
+        
+     });
+
+
+     
 
      $(".players").on("click", "li", function(){
 
@@ -30,14 +51,14 @@ $(document).ready( function() {
     };
 
     var allGolfers = {
-    AdamScott: new Players("Adam Scott",1, "images/flyingtrapeze.jpg"),
-    HenrikStenson: new Players("Henrik Stenson",2, "images/hoop.png"),
-    BubbaWatson: new Players("Bubba Watson ",3, "images/germanwheel.jpg"),
-    TigerWoods: new Players("Tiger Woods",4, "images/handbalancing.jpg"),
-    MattKuchar: new Players("Matt Kuchar",5, "images/firejuggling.png"),
-    RoryMcIlroy: new Players("Rory McIlroy",6, "images/firejuggling.png"),
-    JasonDay: new Players("Jason Day",7, "images/firejuggling.png"),
-    SergioGarcia: new Players("Sergio Garcia",8, "images/firejuggling.png"),
-    JordanSpieth: new Players("Jordan Spieth",9, "images/firejuggling.png"),
-    JustinRose: new Players("Justin Rose",10, "images/firejuggling.png"),
+    AdamScott: new Players("Adam Scott",1, "img/adam_scott.jpg"),
+    HenrikStenson: new Players("Henrik Stenson",2, "img/HenrikStenson.png"),
+    BubbaWatson: new Players("Bubba Watson ",3, "img/BubbaWatson.jpg"),
+    TigerWoods: new Players("Tiger Woods",4, "img/TigerWoods.jpg"),
+    MattKuchar: new Players("Matt Kuchar",5, "img/MattKuchar.jpg"),
+    RoryMcIlroy: new Players("Rory McIlroy",6, "img/RoryMcIlroy.jpg"),
+    JasonDay: new Players("Jason Day",7, "img/JasonDay.jpg"),
+    SergioGarcia: new Players("Sergio Garcia",8, "img/SergioGarcia.jpg"),
+    JordanSpieth: new Players("Jordan Spieth",9, "img/JordanSpieth.jpg"),
+    JustinRose: new Players("Justin Rose",10, "img/JustinRose.jpg")
     };
